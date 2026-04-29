e43a253c5b1a43726544e9f40d0651d9b02ee79e
Ishrat2413 <ishratrintu2413@gmail.com>
2026-04-29 17:41:45 +0600

    feat: add Terms and Conditions page with banner and content components

---
+++ app/(home)/terms-conditions/page.tsx
@@ -0,0 +1,11 @@
+import TermsConditionsBanner from "@/components/terms-conditions/terms-conditions-banner";
+import TermsConditionsContent from "@/components/terms-conditions/terms-conditions-content";
+
+export default function TermsAndConditionsPage() {
+  return (
+    <>
+      <TermsConditionsBanner />
+      <TermsConditionsContent />
+    </>
+  );
+}
