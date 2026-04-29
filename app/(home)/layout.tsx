6b08c0106b280366072a4078d8f43c266b772ab1
FyslAhmd <info.faysal.32@gmail.com>
2026-04-29 15:30:25 +0600

    fix: conditionally render footer based on current pathname

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Footer from "@/components/shared/footer";
 import Navbar from "@/components/shared/navbar";
 
@@ -6,11 +9,16 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const hideFooterRoutes = ["/map"];
+
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <main className="relative z-10 flex-1">{children}</main>
-      <Footer />
+      
+      {!hideFooterRoutes.includes(pathname) && <Footer />}
     </div>
   );
 }
