768693c6eb86e6a621895eaf5277d4ad426a73e0
FyslAhmd <info.faysal.32@gmail.com>
2026-05-19 16:46:55 +0600

    feat: Update ActiveMarker component to change gallery link text to a heading

--- components/map/surf-map-view.tsx
+++ components/map/surf-map-view.tsx
@@ -150,7 +150,7 @@ function ActiveMarker({ spot, icon, onClick }: { spot: SurfSpot; icon: L.DivIcon
                 href={`/gallery?locationId=${spot.id}`}
                 className="inline-flex items-center rounded-sm bg-brand-default px-3 py-1.5 text-xs font-semibold transition-colors"
               >
-                View Gallery
+                <h1 className="text-white">View Gallery</h1>
               </Link>
             </div>
           </div>
